133ea9623b708ba00af08146c1142dfd3cd0723d
BernardinhO <bernardrahani@ymail.com>
2021-05-09 13:14:33 +0200

    rajout de commentaires

--- KarimExos.py
+++ KarimExos.py
@@ -1,7 +1,12 @@
+# article<="papier"
+# Cette application sert à faire une facture
+# Blabalbla Blabalbla Blabalbla Blabalbla Blabalbla
+
 #Stylo a 5€
 #Je recherche le tarif TTC
 #Le taux de TVA est a 20%
 
+# J'assigne (ou initialise) stylo à la variable article
 article="stylo"
 print(type(article))
 prix=5
